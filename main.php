33b3a2889d2e074aab2e4f760351ef0447f5f256
sjoerd akkerman <sakkerman@roc-dev.com>
2016-02-02 13:54:15 +0100

    first commit

---
+++ main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    </head>
+    <body style="background-color:#85e0e0;">
+     <?php 
+$color=array("red","green","yellow","blue");
+    foreach($color as $value){
+    echo "$value<br>";
+}
+?>   
+    </body>
+</html>
